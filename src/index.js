9b5d604c7de97b3c5ef3f3c097d0b36d7ab46bfe
Céleste Robinson <celestekrobinson@gmail.com>
2017-10-28 23:13:53 -0600

    First commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,99 @@
-function add(a, b) {
-  return a + b;
+$(document).ready(function(){ 
+  //on enter runs the submit button function
+  $(document).bind('keypress', function(event) {
+      if(event.keyCode==13){
+           $('#submit').trigger('click');
+      }
+  });
+});
+
+function isValidDate(date) {
+    var date = document.getElementById('user-input').value;
+    console.log(date);
+    if (hasValidLength(date)) {
+console.log(hasValidLength(date));
+        if(hasValidMonth(date)) {
+            if(hasValidDelimiter(date)) {
+                if(hasValidDay(date)) {
+                    document.body.style.backgroundColor = '#2ECC40';
+                } else {
+                    document.body.style.backgroundColor = '#FF4136';
+                }
+                $('#user-input').val('');
+            } else {
+              document.body.style.backgroundColor = '#FF4136';
+            }
+        } else {
+          document.body.style.backgroundColor = '#FF4136';
+        }
+    } else {
+        document.body.style.backgroundColor = '#FF4136';
+    }
+}
+
+function hasValidLength(date) {
+    return date.length === 8 || date.length === 10;
+}
+
+function hasValidMonth(date) {
+    return date.slice(0,2) === '01' || date.slice(0,2) === '02' || date.slice(0,2) === '03' || 
+           date.slice(0,2) === '04' || date.slice(0,2) === '05' || date.slice(0,2) === '06' || 
+           date.slice(0,2) === '07' || date.slice(0,2) === '08' || date.slice(0,2) === '09' || 
+           date.slice(0,2) === '10' || date.slice(0,2) === '11' || date.slice(0,2) === '12';
+}
+
+function hasValidDelimiter(date) {
+    return (date.slice(2,3) === '-' && date.slice(5,6) === '-') ||   
+           (date.slice(2,3) === '/' && date.slice(5,6) === '/') ||
+           (date.slice(2,3) === '.' && date.slice(5,6) === '.');
+}
+
+function hasValidYear(date) {
+
+}
+
+function getDaysInMonth(date) {
+    switch(date[0] + date[1]) {
+      case '01':
+      case '03':
+      case '05':
+      case '07':
+      case '08':
+      case '10':
+      case '12':
+        return 31;
+      case '04':
+      case '06':
+      case '09':
+      case '11':
+        return 30;
+      case '02':
+        if (getYear(date) % 4 === 0) {
+          return 29;
+        }
+        return 28;
+    }
+}  
+
+function getYear(date) {
+    var year;
+    if (date.length === 10) {
+        year = (date[6] + date[7] + date[8] + date[9]);
+    } else if (date.length === 8) {
+        year = (date[6] + date[7]);
+    }
+    return +year;
+}
+
+function getDay(date) {
+    return +(date[3] + date[4]);
+}
+
+function hasValidDay(date) {
+    var daysInMonth = getDaysInMonth(date);
+    var day = getDay(date);
+    if (day <= daysInMonth && day >= 1) {
+      return true;
+    }
+    return false;
 }
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,7 +1,72 @@
-describe('Test', function() {
-  describe('add', function() {
-    it('should add two numbers', function() {
-      expect(add(2, 3)).toBe(5);
+describe('isValidDate', function() {
+    describe('hasValidLength', function() {
+        it('has 8 or 10 characters', function() {
+            expect(hasValidLength('03-13-1991')).toBe(true);
+            expect(hasValidLength('03-13-91')).toBe(true);
+
+            expect(hasValidLength('03-13-19911')).toBe(false);  // 11 chars
+            expect(hasValidLength('03-13-199')).toBe(false);    // 9 chars
+            expect(hasValidLength('03-13-9')).toBe(false);      // 7 chars
+            expect(hasValidLength('2/19/2014')).toBe(false);      // 7 chars
+        });
     });
-  });
+
+    describe('hasValidMonth', function() {
+        it('has a month of "01" - "12"', function() {
+            expect(hasValidMonth('01-13-1991')).toBe(true);
+            expect(hasValidMonth('12-13-91')).toBe(true);
+
+            expect(hasValidMonth('00-13-1991')).toBe(false);
+            expect(hasValidMonth('13-13-1991')).toBe(false);
+        });
+    });
+    describe('hasValidDelimiter', function() {
+        it('has a delimiter of "-" or "/" or "."', function() {
+            expect(hasValidDelimiter('09-10-1992')).toBe(true);
+            expect(hasValidDelimiter('09/10/1992')).toBe(true);
+            expect(hasValidDelimiter('09.10.1992')).toBe(true);
+
+            expect(hasValidDelimiter('09~10~1992')).toBe(false);  // Invalid Delimiter
+            expect(hasValidDelimiter('09\10\1992')).toBe(false);  // Invalid Delimiter
+            expect(hasValidDelimiter('09,10,1992')).toBe(false);  // Invalid Delimiter
+            expect(hasValidDelimiter('09/10-1992')).toBe(false);  // Invalid Delimiter
+            expect(hasValidDelimiter('09-10.1992')).toBe(false);  // Invalid Delimiter
+            expect(hasValidDelimiter('09.10/1992')).toBe(false);  // Invalid Delimiter
+        });
+    });
+    describe('hasValidDay', function() {
+        it('has a valid day given the month', function() {
+            expect(hasValidDay('01-01-1992')).toBe(true);
+            expect(hasValidDay('01-31-1992')).toBe(true);
+            expect(hasValidDay('02-29-1992')).toBe(true);
+            expect(hasValidDay('02-29-92')).toBe(true);
+            expect(hasValidDay('02-28-1992')).toBe(true);
+            expect(hasValidDay('03-31-1992')).toBe(true);
+            expect(hasValidDay('04-30-1992')).toBe(true);
+            expect(hasValidDay('05-31-1992')).toBe(true);
+            expect(hasValidDay('06-30-1992')).toBe(true);
+            expect(hasValidDay('07-31-1992')).toBe(true);
+            expect(hasValidDay('08-31-1992')).toBe(true);
+            expect(hasValidDay('09-30-1992')).toBe(true);
+            expect(hasValidDay('10-31-1992')).toBe(true);
+            expect(hasValidDay('11-30-1992')).toBe(true);
+            expect(hasValidDay('12-31-1992')).toBe(true);
+
+            expect(hasValidDay('01-32-1992')).toBe(false);
+            expect(hasValidDay('01-00-1992')).toBe(false);
+            expect(hasValidDay('02-30-1992')).toBe(false);
+            expect(hasValidDay('02--1-1992')).toBe(false);
+            expect(hasValidDay('03-32-1992')).toBe(false);
+            expect(hasValidDay('04-31-1992')).toBe(false);
+            expect(hasValidDay('05-32-1992')).toBe(false);
+            expect(hasValidDay('06-31-1992')).toBe(false);
+            expect(hasValidDay('07-32-1992')).toBe(false);
+            expect(hasValidDay('08-32-1992')).toBe(false);
+            expect(hasValidDay('09-31-1992')).toBe(false);
+            expect(hasValidDay('10-32-1992')).toBe(false);
+            expect(hasValidDay('11-31-1992')).toBe(false);
+            expect(hasValidDay('12-32-1992')).toBe(false);
+
+        });
+    });
 });
